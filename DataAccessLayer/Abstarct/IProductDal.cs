60950c1648783b8ce55d24a6a06155d87f630ed8
esra.kurt <esra.kurt97833@gmail.com>
2022-09-06 10:37:28 +0300

    Add dataaccess layer

---
+++ DataAccessLayer/Abstarct/IProductDal.cs
@@ -0,0 +1,14 @@
+﻿using EntityLayer.Cocrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstarct
+{
+    public interface IProductDal :IGenericDal<Product>
+    {
+        
+    }
+}
