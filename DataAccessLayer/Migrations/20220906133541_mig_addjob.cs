884cdcfeb7524f28202b08c0b44bc07c5afd8939
esra.kurt <esra.kurt97833@gmail.com>
2022-09-06 16:41:28 +0300

    Update entity layer add job class

---
+++ DataAccessLayer/Migrations/20220906133541_mig_addjob.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class mig_addjob : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "JobId",
+                table: "Customers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Jobs",
+                columns: table => new
+                {
+                    JobId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Jobs", x => x.JobId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_JobId",
+                table: "Customers",
+                column: "JobId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Customers_Jobs_JobId",
+                table: "Customers",
+                column: "JobId",
+                principalTable: "Jobs",
+                principalColumn: "JobId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Customers_Jobs_JobId",
+                table: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "Jobs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Customers_JobId",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "JobId",
+                table: "Customers");
+        }
+    }
+}
--- EntityLayer/Cocrete/Job.cs
+++ EntityLayer/Cocrete/Job.cs
@@ -7,14 +7,11 @@ using System.Threading.Tasks;
 
 namespace EntityLayer.Cocrete
 {
-   public class Customer
+  public   class Job
     {
         [Key]
-        public int Id { get; set; }
+        public int JobId { get; set; }
         public string Name { get; set; }
-        public string City{ get; set; }
-        public int JobId{ get; set; }
-        public Job Job{ get; set; }
-
+        public List <Customer> Customers { get; set; }
     }
 }
