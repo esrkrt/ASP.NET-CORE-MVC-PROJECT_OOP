3bb61d0acad4594c3bf2ecf2ee5fa9154c5ad6f3
esra.kurt <esra.kurt97833@gmail.com>
2022-09-06 14:27:40 +0300

    Add product listining

--- Project_OOP/Controllers/ProductController.cs
+++ Project_OOP/Controllers/ProductController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Cocrete;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +11,23 @@ namespace Project_OOP.Controllers
 {
     public class ProductController : Controller
     {
+
+        ProductManager productmanager = new ProductManager(new EfProductDal());
         public IActionResult Index()
         {
+            var values = productmanager.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddProduct()
+        {
             return View();
+        }
+        [HttpPost]
+        public IActionResult AddProduct(Product p)
+        {
+            productmanager.TInsert(p);
+            return RedirectToAction("Index");
         }
     }
 }
