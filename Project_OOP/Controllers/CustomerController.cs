4deecd8d04f28ac08e505a2790ad05fac93a36fa
esra.kurt <esra.kurt97833@gmail.com>
2022-09-06 15:38:29 +0300

    Add customer listing

---
+++ Project_OOP/Controllers/CustomerController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_OOP.Controllers
+{
+    public class CustomerController : Controller
+    {
+
+       CustomerManager customermanager = new CustomerManager(new EfCustomerDal());
+        public IActionResult Index()
+        {
+            var values = customermanager.TGetList();
+            return View(values);
+        }
+    }
+}
