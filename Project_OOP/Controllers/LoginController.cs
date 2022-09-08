cdbbcc7379b14e7e4da453577abaff1eb907bd85
esra.kurt <esra.kurt97833@gmail.com>
2022-09-08 11:47:24 +0300

    Add login page

---
+++ Project_OOP/Controllers/LoginController.cs
@@ -0,0 +1,45 @@
+﻿using EntityLayer.Cocrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Project_OOP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_OOP.Controllers
+{
+    [AllowAnonymous ]
+    public class LoginController : Controller
+    {
+        private readonly SignInManager<AppUser> _signManager;
+
+        public LoginController(SignInManager<AppUser> signManager)
+        {
+            _signManager = signManager;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task< IActionResult> Index(UserLoginViewModel p)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signManager.PasswordSignInAsync(p.Username, p.Password, false, true);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Category");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Hatalı Kullanıcı adı veya Şifre");
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ Project_OOP/Models/UserLoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_OOP.Models
+{
+    public class UserLoginViewModel
+    {
+        [Required(ErrorMessage = "Lütfen kullanıcı adını giriniz...")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Lütfen şifreyi giriniz...")]
+        public string Password { get; set; }
+    }
+}
